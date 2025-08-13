6bebd2a8db2caa8de1c1c9a8d617311b136e3d5f
Hezbon-Karianjahi <hezbongichemi99@gmail.com>
2025-08-13 16:37:55 +0300

    scipt

---
+++ script.js
@@ -0,0 +1,75 @@
+let firstName ="Hezbon";
+console.log(firstName)
+console.log(typeof(firstName));
+let secondName ="Karianjahi";
+const payDividend = 0.3;
+console.log(1);
+let product = 256*45;
+console.log(product);
+console.log(typeof(product));
+let difference =45-28;
+console.log(difference);
+let a = true
+console.log(a);
+console.log(typeof(a));
+let b = 16;
+console.log(b);
+console.log(typeof(b));
+let mySecondName = "Karianjahi";
+console.log(mySecondName);
+console.log(typeof(mySecondName));
+function add(a, b){
+    return a+b;
+}
+console.log(add);
+console.log(add(7,15));
+var groceries =["Milk", "Egg", "Cheese", "orange"];
+for( var i=0; i<groceries.length; i++){
+        console.log(groceries[i]);
+console.log(typeof(groceries))
+
+}
+const body = document.getElementsByTagName("body")[0];
+function setColor(name){
+    body.style.backgroundColor= name;
+} 
+function submitData(){
+    alert('You have successfully submitted');
+}
+let x=20<30;
+console.log(x);
+let buttonOne = document.getElementById("myButton");
+buttonOne.addEventListener("click",helpMe);
+function helpMe(){
+    alert("Help me whenever i seek help,human!");
+}
+let h1Element=document.getElementById('my-header');
+h1Element.innerHTML= "The Gnome are real people"
+const w= 30;
+if(w==10){
+    console.log("is True");
+} else{
+    console.log("is not True");
+}
+const fruits=['mango','orange','pineapple,passion','Thornmelon'];
+console.log(fruits);
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i])};
+for(let i=0;i<5;i++){
+    console.log('Hezzy'+i);}
+    let names=['John','james','Michael','Joshua'];
+    console.log(names);
+    names[4]="Mary";
+    names[5]='Brayoo';
+    console.log(names.length);
+    
+
+
+
+
+
+
+
+    
+
+
